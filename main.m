52fc10801a435be06059a362152e5421f461d4b4
huberywooooo <hbw8456@163.com>
2024-10-14 14:14:06 +0800

    main

---
+++ main.m
@@ -0,0 +1,14 @@
+%%%%-----------------------------openGeodyna--------------------------------------
+%
+%       Description:
+
+%
+%   openGeodyna: An Open Source Library for Geophysical dynamics
+%   Author(s): Hubery H.B. Woo (hbw8456@163.com)
+%   Copyright 2023-2024 Chongqing Three Gorges University
+
+
+function main()
+	disp("unfinished.");
+
+end
